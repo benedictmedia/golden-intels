9c4c768d99c976b6000dfa99fcb3a44b6f8ad6ac
Gideon Dotse <dotsegideon2@gmail.com>
2026-06-01 18:51:47 +0000

    Render log errors fixed

--- server/src/routes/notifications.js
+++ server/src/routes/notifications.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const { getNotifications, markAsRead } = require('../controllers/notificationController');
-const { protect } = require('../middleware/authMiddleware');
+const protect = require('../middleware/authMiddleware');
 
+// Make sure protect is correctly imported as middleware
 router.get('/', protect, getNotifications);
 router.put('/:id/read', protect, markAsRead);
 
